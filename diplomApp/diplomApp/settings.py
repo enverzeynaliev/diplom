5883404cecd5638e1e8623f535d15bdc776bbf6b
enverzeynaliev <fizrukcom62@gmail.com>
2022-05-05 23:21:45 +0300

    added db file

--- diplomApp/diplomApp/settings.py
+++ diplomApp/diplomApp/settings.py
@@ -77,6 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '3306',
     }
 }
 
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Russia/Moscow'
 
 USE_I18N = True
 
